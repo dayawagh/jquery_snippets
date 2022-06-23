f07f97bfe9323cc5e329a9db2989438062afa649
dayawagh <dayawagh54@gmail.com>
2022-06-23 18:48:32 +0530

    Create get_static_file_path.js

---
+++ get_static_file_path.js
@@ -0,0 +1,13 @@
+$(document).ready(() => {
+    $("#video").change(function () {
+        const file = this.files[0];
+        if (file) {
+            let reader = new FileReader();
+            reader.onload = function (event) {
+                console.log(event.target.result);
+                $("#video_preview").attr("src", event.target.result);
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+});
