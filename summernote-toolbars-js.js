cf2b88f5e9c799465de771cb963840a8586a1557
W Daya <dayawagh54@gmail.com>
2023-03-09 17:42:43 +0530

    summernote-toolbars-js.js
    summernote-toolbars-js.js

---
+++ summernote-toolbars-js.js
@@ -0,0 +1,11 @@
+$('#summernote').summernote({
+  toolbar: [
+    // [groupName, [list of button]]
+    ['style', ['bold', 'italic', 'underline', 'clear']],
+    ['font', ['strikethrough', 'superscript', 'subscript']],
+    ['fontsize', ['fontsize']],
+    ['color', ['color']],
+    ['para', ['ul', 'ol', 'paragraph']],
+    ['height', ['height']]
+  ]
+});
